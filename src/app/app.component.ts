ca63f0bce9e301abe4760eb7c3ccf5e137059d6a
Kisan5 <kisansenapati13@gmail.com>
2024-12-15 11:07:58 +0530

    Created dashboard component, added the structure, imported bootstrap, used one standalone component in another i.e app component and set default margin padding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,DashboardComponent], //imported DashboardComponent to use in this component. used in html file check
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
