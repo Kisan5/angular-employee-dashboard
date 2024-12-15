f4d46e7b651c8fe5dda51fa42ac6f611bbcec02b
Kisan5 <kisansenapati13@gmail.com>
2024-12-15 21:17:42 +0530

    http client used, called api of json-server to get employee details to show on table dynamically, added scroll div without scroll bar

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule], //kisan: to use ngFor need to add commonModule
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  http!:HttpClient;
+  respondedEmployeeRecordsArray: any;
 
+  constructor(http:HttpClient){
+    this.http=http;
+    this.getEmployees();
+  }
+  getEmployees(){
+    this.http.get("http://localhost:3000/employees").subscribe(item => this.respondedEmployeeRecordsArray=item);
+  }
 }
